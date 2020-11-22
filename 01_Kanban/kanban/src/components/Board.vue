a6f07dfe0f63c3f6af061f213582e593a93bc0e9
Laércio Oliveira <jose.laercio.bezerra@gmail.com>
2020-11-22 11:33:20 -0300

    chore: add vue-fragment

--- 01_Kanban/kanban/src/components/Board.vue
+++ 01_Kanban/kanban/src/components/Board.vue
@@ -1,14 +1,19 @@
 <template>
-  <ul class="container-board">
-    <List :title="'Tarefas'" />
-  </ul>
+  <fragment>
+    <button>add column</button>
+    <ul class="container-board">
+      <List :title="'Tarefas'" />
+    </ul>
+  </fragment>
 </template>
 
 <script>
+import { Fragment } from "vue-fragment";
 import List from "./List.vue";
 export default {
   components: {
     List,
+    Fragment,
   },
   data() {
     return {
